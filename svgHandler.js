4d8143e9390ea460dd45863c16be78861ede9278
oroafrica <moegamat@oroafrica.com>
2021-06-21 08:37:43 +0200

    skeleton-init

--- svgHandler.js
+++ svgHandler.js
@@ -1,10 +1,31 @@
 (()=>
 {
+    const { log, trace, error, warn, info, clear } = console;
+    const parser = new DOMParser();
+    const svg = $("#svgHost");
+    const _input = $(":text");
+    let doc, svgString, serializer = new XMLSerializer();
+
     const test =()=>
     {
         console.clear();
         console.log("svgHandler is calling: ",svgInput.classic()); 
+        console.log("svgHandler is calling dynamically: ",svgInput["classic"]()); 
     }
     
-    test();
+    //###########################################################################
+    const render=()=>
+    {
+        let comp=
+        {
+            test:()=> log("svgHandler is calling dynamically: ",svgInput["classic"]())
+            ,msg:()=> info(document.title)
+        }
+        
+        Object.values(comp).forEach(f=>f()); //iterator
+    };
+
+    render(); //executable
+    //###########################################################################
+
 })();
